fc984aa647ee51ce64464678cc16a8008942917d
ipsitapanda123 <ip.ipsitapanda@gmail.com>
2023-06-05 11:29:19 +0530

    new java file GitihubPushDemo added

---
+++ src/test/java/com/actitime/testcases/TC4_GitihubPushDemo.java
@@ -0,0 +1,13 @@
+package com.actitime.testcases;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC4_GitihubPushDemo 
+{
+	@Test
+	public void git()
+	{
+		Reporter.log("git push done" , true);
+	}
+}
